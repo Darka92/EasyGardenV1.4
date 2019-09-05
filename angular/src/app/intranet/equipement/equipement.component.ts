edd38cac1d704e5cc42ecfbb37cde14119f13571
LefevreEmmanuel <waiasiaa@protonmail.com>
2019-09-05 10:10:48 +0200

    Gestion du bouton retour vers l'entité précedement sélectionnée,
    fonction goBack

--- angular/src/app/intranet/equipement/equipement.component.ts
+++ angular/src/app/intranet/equipement/equipement.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-equipement',
   templateUrl: './equipement.component.html',
   styleUrls: ['./equipement.component.css']
 })
+
 export class EquipementComponent implements OnInit {
 
-    constructor(public router: Router) { }
+    constructor(private router: Router, private location: Location) {}
+
+  goBack() {
+    this.location.back();
+  }
 
   ngOnInit() {
   }
