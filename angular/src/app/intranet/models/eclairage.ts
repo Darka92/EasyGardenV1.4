ff1927ea48bf54d032af8fd3170bbd2ee17c7f46
Darka <waiasiaa2@protonmail.com>
2019-10-16 00:45:34 +0200

    Ang5: Maj du mvc Eclairage

--- angular/src/app/intranet/models/eclairage.ts
+++ angular/src/app/intranet/models/eclairage.ts
@@ -4,7 +4,7 @@
 
    export class Eclairage {
 
-    constructor(public eclairageId : number, public nom :string, public localisation:string, public capteurDefautAmpoule: string, public capteurLuminosite: string, public statut: string) {}
+    constructor(public eclairageId : number, public nom :string, public localisation:string, public capteurDefautAmpoule: boolean, public capteurLuminosite: number, public statut: boolean) {}
 
     /*public eclairageId: number;
     public nom: string;
--- angular/src/app/services/eclairages.service.ts
+++ angular/src/app/services/eclairages.service.ts
@@ -18,14 +18,14 @@ export class EclairagesService {
 
   constructor() {
 
-    let eclairages1: Eclairage = new Eclairage (1,'Secteur 1','Allée', '0', '34205', '1');
-    let eclairages2: Eclairage = new Eclairage (2,'Secteur 2','Façade', '1', '30007', '0');
-    let eclairages3: Eclairage = new Eclairage (3,'Secteur 3','Perron', '0', '36548', '1');
-    let eclairages4: Eclairage = new Eclairage (4,'Secteur 4','Arbres', '0', '37841', '1');
-    let eclairages5: Eclairage = new Eclairage (5,'Secteur 5','Palmiers', '0', '39148', '0');
-    let eclairages6: Eclairage = new Eclairage (6,'Secteur 6','Massif bassin', '0', '36218', '0');
-    let eclairages7: Eclairage = new Eclairage (7,'Secteur 7','Massif avant', '1', '33210', '1');
-    let eclairages8: Eclairage = new Eclairage (8,'Secteur 8','Portail', '0', '35498', '1');
+    let eclairages1: Eclairage = new Eclairage (1,'Secteur 1','Allée', false, 34205, true);
+    let eclairages2: Eclairage = new Eclairage (2,'Secteur 2','Façade', true, 30007, false);
+    let eclairages3: Eclairage = new Eclairage (3,'Secteur 3','Perron', false, 36548, true);
+    let eclairages4: Eclairage = new Eclairage (4,'Secteur 4','Arbres', false, 37841, true);
+    let eclairages5: Eclairage = new Eclairage (5,'Secteur 5','Palmiers', true, 39148, false);
+    let eclairages6: Eclairage = new Eclairage (6,'Secteur 6','Massif bassin', false, 36218, false);
+    let eclairages7: Eclairage = new Eclairage (7,'Secteur 7','Massif avant', false, 33210, true);
+    let eclairages8: Eclairage = new Eclairage (8,'Secteur 8','Portail', false, 35498, true);
 
     this.eclairages.push(eclairages1);
     this.eclairages.push(eclairages2);
@@ -49,11 +49,11 @@ export class EclairagesService {
   };
 
   switchOnOne(i: number) {
-    this.eclairages[i].statut = '1';
+    this.eclairages[i].statut = true;
   }
 
   switchOffOne(i: number) {
-    this.eclairages[i].statut = '0';
+    this.eclairages[i].statut = false;
   }
 
 
